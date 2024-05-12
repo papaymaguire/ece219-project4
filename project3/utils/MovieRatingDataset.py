1ef3486bc943f54fcd9ac2754b63a424960a14dd
Maguire Papay <maguire.papay@scalez.org>
2024-05-12 09:49:15 -0700

    refactor dataset to class

---
+++ project3/utils/MovieRatingDataset.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from surprise.dataset import Dataset
+from surprise.reader import Reader
+
+class MovieRatingsDataset ():
+
+    def __init__(self) -> None:
+        self.table, raws = self.ingest_data()
+        ratings = raws.pop('ratings')
+        self.ratings = ratings
+        self.metadata = raws
+        self.og_dataset = self.dataset_from_df(ratings)
+        self.popular_dataset = self.dataset_from_df(self.popular_trimming(ratings))
+        self.unpopular_dataset = self.dataset_from_df(self.unpopular_trimming(ratings))
+        # self.high_variance_dataset = self.dataset_from_df(self.high_variance_trimming(ratings))
+
+    def format_ratings(self, ratings_df: pd.DataFrame):
+        return ratings_df[['userId', 'movieId', 'rating']]
+
+    def ingest_data(self):
+        links = pd.read_csv('../data/links.csv')
+        movies = pd.read_csv('../data/movies.csv')
+        ratings = self.format_ratings(pd.read_csv('../data/ratings.csv'))
+        tags = pd.read_csv('../data/tags.csv')
+        raws = {
+            "links": links,
+            "movies": movies,
+            "ratings": ratings,
+            "tags": tags
+        }
+        table = ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0).to_numpy()
+        return table, raws
+
+
+    def popular_trimming(self, ratings_df: pd.DataFrame):
+        popular_movies = ratings_df['movieId'].value_counts() > 2
+        return ratings_df[ratings_df['movieId'].isin(popular_movies)]
+
+    def unpopular_trimming(self, ratings_df: pd.DataFrame):
+        unpopular_movies = ratings_df['movieId'].value_counts() <= 2
+        return ratings_df[ratings_df['movieId'].isin(unpopular_movies)]
+
+    def high_variance_trimming(self, ratings_df: pd.DataFrame):
+        pass
+
+
+    def dataset_from_df(self, ratings_df):
+        reader = Reader(rating_scale=(0.5, 5))
+        dataset = Dataset.load_from_df(ratings_df, reader)
+        return dataset
+
+    
